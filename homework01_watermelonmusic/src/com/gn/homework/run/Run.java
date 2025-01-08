d6460ac609c859711c4699aa8374d957a36bdde3
권용규 <@LAPTOP-H0FP6QDH>
2025-01-08 23:11:04 +0900

    [KYG] 과제1 최초서밋

---
+++ homework01_watermelonmusic/src/com/gn/homework/run/Run.java
@@ -0,0 +1,13 @@
+package com.gn.homework.run;
+
+import com.gn.homework.view.MusicMenu;
+
+public class Run {
+
+	public static void main(String[] args) {
+		MusicMenu mm = new MusicMenu();
+		mm.mainMenu();
+
+	}
+
+}
---
+++ homework01_watermelonmusic/src/com/gn/homework/view/MusicMenu.java
@@ -0,0 +1,12 @@
+package com.gn.homework.view;
+
+public class MusicMenu {
+	
+	public void mainMenu() {
+		System.out.println("환영합니다!!");
+		
+		
+		
+	}
+
+}
